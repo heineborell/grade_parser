ff9615c28143e8c05cde682bbdcd7924f60a69e7
DENIZ DEVECIOGLU <dodeve@gmail.com>
2025-06-21 18:19:05 +0900

    fin form

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import tools
 
 def main():
     # Load your LMS datasets
-    df_orig = "gc_2025CMN17.10BS103a02_fullgc_2025-06-20-14-17-04.csv"
-    announced_grades_path = "test_2.xls"
+    df_orig = "data/originals/gc_2025CMN17.10BS103a02_fullgc_2025-06-21-18-05-41.csv"
+    announced_grades_path = "data/originals/base_2.xls"
+    # section3
+    # df_orig = "data/originals/gc_2025CMN17.10BS103a03_fullgc_2025-06-21-18-13-43.csv"
+    # announced_grades_path = "data/originals/base_3.xls"
     lms_graded = tools.lms_grader(df_orig)
 
     # save the dataframes
-    with open("weighted_2.csv", "w", encoding="utf-8", newline="") as f:
+    with open("data/weighted_2.csv", "w", encoding="utf-8", newline="") as f:
         lms_graded[0].to_csv(f, index=False)
     subprocess.run(
         ["vd", "-f", "csv", "-"],
@@ -17,7 +20,7 @@ def main():
         text=True,
     )
 
-    ann_2 = tools.lms_to_announced(announced_grades_path, df_orig, "announced_2.xls")
+    ann = tools.lms_to_announced(announced_grades_path, df_orig, "data/announced_2.xls")
     # subprocess.run(["vd", "-f", "csv", "-"], input=ann_2.to_csv(index=False), text=True)
 
     # plot the grade distribution
